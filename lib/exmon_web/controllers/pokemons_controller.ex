4d759f3eba70f16e692625d2ad61597604c2d9d8
wagner carvalho <wagner.carvalho@credisis.com.br>
2022-03-25 13:13:07 -0300

    refatoracao controller pokemons

--- lib/exmon_web/controllers/pokemons_controller.ex
+++ lib/exmon_web/controllers/pokemons_controller.ex
@@ -4,15 +4,10 @@ defmodule ExmonWeb.PokemonsController do
   action_fallback ExmonWeb.FallbackController
 
   def show(conn, %{"name" => name}) do
-    name
-    |> Exmon.fetch_pokemon()
-    |> handle_response(conn)
+    with {:ok, pokemon} <- Exmon.fetch_pokemon(name) do
+      conn
+      |> put_status(:ok)
+      |> json(pokemon)
+    end
   end
-
-  defp handle_response({:ok, pokemon}, conn) do
-    conn
-    |> put_status(:ok)
-    |> json(pokemon)
-  end
-  defp handle_response({:error, _reason} = error, _conn), do: error
 end
