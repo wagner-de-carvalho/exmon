77037dc4581569e254a8377568123d057c8c5f7f
wagner carvalho <wagner.carvalho@credisis.com.br>
2022-03-25 15:53:27 -0300

    create pokemon

--- .iex.exs
+++ .iex.exs
@@ -1,8 +1,20 @@
 alias Exmon.Trainer
-alias Exmon.Repo
-alias Exmon.Pokemon
+alias Exmon.{Pokemon, Repo}
+alias Exmon.Trainer.Pokemon, as: TrainerPokemon
 alias Exmon.PokeApi.Client
-alias Exmon.Trainer.Pokemon
+alias Exmon.Trainer.Pokemon.Create
+
 params = %{name: "Exmon", password: "12345678"}
 params2 = %{name: "Exmon"}
-pokemon = %{name: "pikachu", nickname: "raio", weight: 35, types: ["electric"], trainer_id: "271a8632-c9e7-41cd-9e82-8e3d91797509"}
+param_create_pokemon = %{"name" => "pikachu", "nickname" => "raio", "trainer_id" => "271a8632-c9e7-41cd-9e82-8e3d91797509"}
+param_create_pokemon2 = %{"name" => "scyther", "nickname" => "fogo", "trainer_id" => "271a8632-c9e7-41cd-9e82-8e3d91797509"}
+param_create_pokemon3 = %{"name" => "nono", "nickname" => "fogo", "trainer_id" => "271a8632-c9e7-41cd-9e82-8e3d91797509"}
+param_create_pokemon4 = %{"name" => "pikachu", "nickname" => "raio", "trainer_id" => "271a8632-c9e7-41cd-9e82-8e3d91797501"}
+
+pokemon = %{
+  name: "pikachu",
+  nickname: "raio",
+  weight: 35,
+  types: ["electric"],
+  trainer_id: "271a8632-c9e7-41cd-9e82-8e3d91797509"
+}
---
+++ lib/exmon/trainer/pokemon/create.ex
@@ -0,0 +1,41 @@
+defmodule Exmon.Trainer.Pokemon.Create do
+  alias Exmon.PokeApi.Client
+  alias Exmon.{Pokemon, Repo, Trainer}
+  alias Exmon.Trainer.Pokemon, as: TrainerPokemon
+
+  def call(%{"name" => name} = params) do
+    name
+    |> Client.get_pokemon()
+    |> handle_response(params)
+  end
+
+  defp handle_response({:ok, body}, %{"trainer_id" => trainer_id} = params) do
+    body
+    |> Pokemon.build()
+    |> check_trainer_id(trainer_id)
+    |> create_pokemon(params)
+  end
+
+  defp handle_response({:error, _reason} = error, _params), do: error
+
+  defp check_trainer_id(pokemon, trainer_id) do
+   case Trainer.Get.call(trainer_id) do
+    {:ok, _trainer} -> pokemon
+    {:error, _reason} = error -> error
+   end
+  end
+
+  defp create_pokemon(%Pokemon{name: name, weight: weight, types: types}, %{
+         "nickname" => nickname,
+         "trainer_id" => trainer_id
+       }) do
+    params = %{name: name, weight: weight, types: types, nickname: nickname, trainer_id: trainer_id}
+    params
+    |> TrainerPokemon.build()
+    |> handle_build()
+  end
+  defp create_pokemon({:error, _reason} = error, _params), do: error
+
+  defp handle_build({:ok, pokemon}), do: Repo.insert(pokemon)
+  defp handle_build({:error, _changeset} = error), do: error
+end
