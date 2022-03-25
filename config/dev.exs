0f8e5032c97f48a5950883393d46c64e5772580c
wagner carvalho <wagner.carvalho@credisis.com.br>
2022-03-25 10:47:31 -0300

    alteracao de porta

--- config/dev.exs
+++ config/dev.exs
@@ -6,7 +6,7 @@ config :exmon, Exmon.Repo,
   password: "postgres",
   hostname: "localhost",
   database: "exmon_dev",
-  port: 5433,
+  # port: 5433,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
