78ab4f2ea87392a476818d65bc575ba7dccbc787
wagner carvalho <wagner.carvalho@credisis.com.br>
2022-03-27 14:34:36 -0300

    correcao view trainer, token adicionado

--- lib/exmon_web/views/trainers_view.ex
+++ lib/exmon_web/views/trainers_view.ex
@@ -1,5 +1,5 @@
 defmodule ExmonWeb.TrainersView do
   use ExmonWeb, :view
 
-  def render("trainer.json", %{trainer: trainer}), do: %{trainer: trainer}
+  def render("trainer.json", %{trainer: trainer, token: token}), do: %{trainer: trainer, token: token}
 end
